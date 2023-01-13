52e3d435f2af57140b578f83d42a2f5835a2751b
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2023-01-13 14:26:44 +0100

    Correction et suite

--- ACT_5/ACT_5-Calculette_binaire/ACT_5-Calculette_binaire/MainWindow.xaml.cs
+++ ACT_5/ACT_5-Calculette_binaire/ACT_5-Calculette_binaire/MainWindow.xaml.cs
@@ -81,7 +81,7 @@ namespace ACT_5_Calculette_binaire
             {
                 result.Text = "";
                 ushort[] nbr1 = RemplirTableau(nbrB1.Text);
-                ushort[] nbr2 = RemplirTableau(nbrB1.Text);
+                ushort[] nbr2 = RemplirTableau(nbrB2.Text);
                 bool ok;
                 ushort[] tRes;
                 
@@ -135,7 +135,7 @@ namespace ACT_5_Calculette_binaire
             {
                 res = (ushort)(t1[i] + t2[i] + report);
 
-                if(res/2 == 0)
+                if((int)res/2 == 0)
                 {
                     report = 0;
                 }
@@ -153,16 +153,18 @@ namespace ACT_5_Calculette_binaire
                     if(res % 2 == 1)
                     {
                         tRes[i] = 1;
-                    }
+                    } 
                     else
                     {
                         tRes[i] = 0;
                     }
                 }
-                if(report == 1)
-                {
-                    ok = false;
-                }
+                
+
+            }
+            if (report == 1)
+            {
+                ok = false;
             }
         }
 
@@ -170,7 +172,6 @@ namespace ACT_5_Calculette_binaire
         {
             int[] tTemp = new int[8];
             tRes = new ushort[8];
-
             bool ok = true;
 
             for (int i = 0; i < 8; i++)
@@ -178,19 +179,20 @@ namespace ACT_5_Calculette_binaire
                 tTemp[i] = t1[i] - t2[i];
             }
 
-            for(int j = 0; j > -1; j--)
+
+            for (int i = 7; i > -0; i--)
             {
-                if(tTemp[j] == -1)
+                if(tTemp[i] == -1)
                 {
-                    t2[j - 1] = (ushort)(t2[j - 1] + 1);
-                    t1[j] = (ushort)(t1[j] + 2);
+                    t2[i - 1]++;
+                    t1[i] += 2;
                 }
 
-                if(t1[j] < t2[j])
+                if(t1[i] < t2[i])
                 {
-                    t2[j - 1] = (ushort)(t2[j - 1] + 1);
-                    t1[j] = (ushort)(t1[j] + 2);
-                    tRes[j] = (ushort)(t1[j] - t2[j]);
+                    t2[i - 1]++;
+                    t1[i] += 2;
+                    tRes[i] = (ushort)(t1[i] - t2[i]);
                 }
             }
 
