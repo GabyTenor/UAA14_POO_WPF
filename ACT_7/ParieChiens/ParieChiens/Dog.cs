0fa4f371f21455cc760df472d4b701b4c140038e
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2023-03-07 14:42:04 +0100

    Pari

---
+++ ACT_7/ParieChiens/ParieChiens/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParieChiens
+{
+    class Dog
+    {
+        private int _number;
+        private double[] _position;
+
+        public Dog(int number)
+        {
+            this._number = number;
+            this._position = new double[2];
+        }
+
+        public void Move()
+        {
+
+        }
+
+        public int Number
+        {
+            get { return _number; }
+            set { _number = value; }
+        }
+
+        public double[] Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+    }
+}
---
+++ ACT_7/ParieChiens/ParieChiens/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParieChiens
+{
+    class User
+    {
+        private string _name;
+        private int _money;
+
+        public User(string name, int money)
+        {
+            this._name = name;
+            this._money = money;
+        }
+
+        public void PlaceTheBet()
+        {
+
+        }
+
+        public void RemoveMoney()
+        {
+
+        }
+
+        public void AddMoney()
+        {
+
+        }
+        
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Money
+        {
+            get { return _money; }
+            set { _money = value; }
+        }
+
+    }
+}
