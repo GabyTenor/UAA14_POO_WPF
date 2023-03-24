4bea2010ac90c12d64a9076511744177c8d03068
Tarnus Gabriel <200198@CCM-Asty-Moulin.local>
2023-03-24 14:25:28 +0100

    Suite + placement

--- ACT_7/ParieChiens/ParieChiens/Dog.cs
+++ ACT_7/ParieChiens/ParieChiens/Dog.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+
 
 namespace ParieChiens
 {
@@ -13,16 +15,36 @@ namespace ParieChiens
         BitmapImage _imageChien = new BitmapImage();
         int[] _positionCourante = new int[2];
         bool _gagne;
-
-        public Dog(int numChien)
+         
+        public Dog(int numChien, int[] positionCourante)
         {
             this._longueurPiste = 1500;
             
             this._numChien = numChien;
+
+            this._positionCourante = positionCourante;
             
-            this._imageChien.EndInit();
             this._imageChien.BeginInit();
             this._imageChien.UriSource = new Uri("/dog.png", UriKind.Relative);
+            this._imageChien.EndInit();
+
+            MainWindow plateau = (ParieChiens.MainWindow)App.Current.MainWindow;
+
+            Image monImage = new Image();
+            monImage.Source = _imageChien;
+            monImage.Stretch = System.Windows.Media.Stretch.None;
+
+            Canvas.SetLeft(monImage, _positionCourante[0]);
+            Canvas.SetTop(monImage, _positionCourante[1]);
+            plateau.circuit.Children.Add(monImage);
+
         }
+
+        public BitmapImage ImageChien
+        {
+            get { return _imageChien; }
+            set { _imageChien = value; }
+        }
+
     }
 }
--- ACT_7/ParieChiens/ParieChiens/MainWindow.xaml.cs
+++ ACT_7/ParieChiens/ParieChiens/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace ParieChiens
 {
     /// <summary>
@@ -20,16 +19,97 @@ namespace ParieChiens
     /// </summary>
     public partial class MainWindow : Window
     {
+        Dog chien1 = new Dog(1, new int[2] { 0, 0});
+        Dog chien2 = new Dog(2, new int[2] { 0, 0 });
+        Dog chien3 = new Dog(3, new int[2] { 0, 0 });
+        Dog chien4 = new Dog(4, new int[2] { 0, 0 });
+
         public MainWindow()
         {
             InitializeComponent();
+            mise.PreviewTextInput += new TextCompositionEventHandler(PreviewMise);
+            numChien.PreviewTextInput += new TextCompositionEventHandler(PreviewNumChien);
+
+        }
+        public void PreviewMise(object sender, TextCompositionEventArgs e)
+        {
+            if (!EstEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                if(((TextBox)sender).Text.Length > 2)
+                {
+                    e.Handled = true;
+                }
+            }
+            
         }
 
-        public void PlacerImage()
+        public void PreviewNumChien(object sender, TextCompositionEventArgs e)
+        {
+            if (!EstEntier(e.Text))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                if(((TextBox)sender).Text.Length > 0)
+                {
+                    e.Handled = true;
+                }
+                switch (e.Text)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+
+                        break;
+
+                    case "3":
+
+                        break;
+
+                    case "4":
+
+                        break;
+
+                    default:
+
+                        e.Handled = true;
+                    break;
+                }
+            }
+        }
+
+        public void PlacerChien()
         {
             Image monImage = new Image();
-            //monImage.Source = imageChien;
+            monImage.Source = chien1.ImageChien;
             monImage.Stretch = System.Windows.Media.Stretch.None;
+
+
+        }
+
+        public bool EstEntier(string text)
+        {
+            bool isInt;
+
+            int entier;
+
+            if (int.TryParse(text, out entier))
+            {
+                isInt = true;
+            }
+            else
+            {
+                isInt = false;
+            }
+
+            return isInt;
         }
     }
 }
